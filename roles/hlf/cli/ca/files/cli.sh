5147d79343dc576422b110b234af3e2d49c2ec2b
achak1987 <chakravorty.antorweep@gmail.com>
2020-07-30 12:01:50 +0000

    refactoring

--- roles/hlf/cli/ca/files/cli.sh
+++ roles/hlf/cli/ca/files/cli.sh
@@ -1,53 +1,52 @@
-idx=$1
-type=$2
+type=$1
 
 # These are static values, for each CA types
 tlsca="tls"
 orgca="org"
 
 export FABRIC_CA_CLIENT_TLS_CERTFILES=$HOST_HOME/ca-cert.pem
-export FABRIC_CA_CLIENT_HOME=$HOST_HOME/caadmin
 
-if [$idx == 0]; then
-  
-  echo "Enroll CA admin for $FABRIC_CA_NAME"
+if [$IDX == 0]; then
+  export FABRIC_CA_CLIENT_HOME=$HOST_HOME/caadmin
+  echo "Enroll CA root for $FABRIC_CA_NAME"
   fabric-ca-client enroll -d -u https://ca-admin-$FABRIC_CA_NAME:$FABRIC_CA_SECRET@$FABRIC_CA_NAME:$FABRIC_CA_PORT
+    
+  echo "Register admin ($ADMIN_USER) at $FABRIC_CA_NAME"
+  fabric-ca-client register -d --id.name $ADMIN_USER --id.secret $ADMIN_SECRET --id.type admin --id.attrs "hf.Registrar.Roles=client,hf.Registrar.Attributes=*,hf.Revoker=true,hf.GenCRL=true,admin=true:ecert,abac.init=true:ecert" -u https://$FABRIC_CA_NAME:$FABRIC_CA_PORT
 
-elif [ $idx != 0 && $type == $tlsca ]; then     
-  echo "Regsiter agent $AGENT_HOST of type $AGENT_TYPE at $FABRIC_CA_NAME"
+  echo "Enroll admin ($ADMIN_USER) for $FABRIC_CA_NAME"
+  export FABRIC_CA_CLIENT_HOME=$HOST_HOME/$ADMIN_USER  
+  fabric-ca-client enroll -d -u https://$ADMIN_USER:$ADMIN_SECRET@$FABRIC_CA_NAME:$FABRIC_CA_PORT
+
+fi
+
+if [ $type == $tlsca ]; then      
+  # We make sure that we are pointed to the admin user, prior to registering agents
+  export FABRIC_CA_CLIENT_HOME=$HOST_HOME/$ADMIN_USER
+  echo "Register agent $AGENT_HOST of type $AGENT_TYPE at $FABRIC_CA_NAME"
   fabric-ca-client register -d --id.name $AGENT_HOST --id.secret $AGENT_SECRET --id.type $AGENT_TYPE -u https://$FABRIC_CA_NAME:$FABRIC_CA_PORT
-  
-  echo "Regsiter peers at $FABRIC_CA_NAME"
-  fabric-ca-client register -d --id.name $PEER2_HOST --id.secret $PEER2_SECRET --id.type peer -u https://$FABRIC_CA_NAME:$FABRIC_CA_PORT
-  fabric-ca-client register -d --id.name $ORDERER_HOST --id.secret $ORDERER_SECRET --id.type peer -u https://$FABRIC_CA_NAME:$FABRIC_CA_PORT
-
-  export FABRIC_CA_CLIENT_MSPDIR=tls-msp
+    
   # Enroll Agent
-  export FABRIC_CA_CLIENT_HOME=$HOST_HOME/peer1
-  fabric-ca-client enroll -d -u https://$PEER1_HOST:$PEER1_SECRET@$FABRIC_CA_NAME:$FABRIC_CA_PORT --enrollment.profile tls --csr.hosts $PEER1_HOST,127.0.0.1
+  export FABRIC_CA_CLIENT_MSPDIR=tls-msp
+  export FABRIC_CA_CLIENT_HOME=$HOST_HOME/$AGENT_HOST
+  fabric-ca-client enroll -d -u https://$AGENT_HOST:$AGENT_SECRET@$FABRIC_CA_NAME:$FABRIC_CA_PORT --enrollment.profile tls --csr.hosts $AGENT_HOST,127.0.0.1
   filename=$(ls $FABRIC_CA_CLIENT_HOME/$FABRIC_CA_CLIENT_MSPDIR/keystore | sort -n | head -1)
   mv $FABRIC_CA_CLIENT_HOME/$FABRIC_CA_CLIENT_MSPDIR/keystore/$filename $FABRIC_CA_CLIENT_HOME/$FABRIC_CA_CLIENT_MSPDIR/keystore/key.pem
   
-elif [ $idx != 0 && $type == $orgca ]; then
-  echo "Regsiter admin at $FABRIC_CA_NAME"
-  fabric-ca-client register -d --id.name admin-$FABRIC_CA_NAME --id.secret $FABRIC_CA_SECRET --id.type admin --id.attrs "hf.Registrar.Roles=client,hf.Registrar.Attributes=*,hf.Revoker=true,hf.GenCRL=true,admin=true:ecert,abac.init=true:ecert" -u https://$FABRIC_CA_NAME:$FABRIC_CA_PORT
-    
-  export FABRIC_CA_CLIENT_MSPDIR=msp
+elif [ $type == $orgca ]; then
+  # We make sure that we are pointed to the admin user, prior to registering agents
+  export FABRIC_CA_CLIENT_HOME=$HOST_HOME/$ADMIN_USER
+  echo "Register agent $AGENT_HOST of type $AGENT_TYPE at $FABRIC_CA_NAME"
+  fabric-ca-client register -d --id.name $AGENT_HOST --id.secret $AGENT_SECRET --id.type $AGENT_TYPE -u https://$FABRIC_CA_NAME:$FABRIC_CA_PORT
+
   # Enroll Agent 
-  export FABRIC_CA_CLIENT_HOME=$HOST_HOME/peer1
-  fabric-ca-client enroll -d -u https://$PEER1_HOST:$PEER1_SECRET@$FABRIC_CA_NAME:$FABRIC_CA_PORT
-  
-  #Enroll Admin
-  export FABRIC_CA_CLIENT_HOME=$HOST_HOME/admin  
-  fabric-ca-client enroll -d -u https://admin-$FABRIC_CA_NAME:$FABRIC_CA_SECRET@$FABRIC_CA_NAME:$FABRIC_CA_PORT
+  export FABRIC_CA_CLIENT_MSPDIR=msp
+  export FABRIC_CA_CLIENT_HOME=$HOST_HOME/$AGENT_HOST
+  fabric-ca-client enroll -d -u https://$AGENT_HOST:$AGENT_SECRET@$FABRIC_CA_NAME:$FABRIC_CA_PORT    
 
   # Transfer admincerts
-  mkdir $HOST_HOME/$PEER1_HOST/msp/admincerts
-  mkdir $HOST_HOME/$PEER2_HOST/msp/admincerts
-  mkdir $HOST_HOME/$ORDERER_HOST/msp/admincerts
-  cp $HOST_HOME/admin/msp/signcerts/cert.pem $HOST_HOME/$PEER1_HOST/msp/admincerts/admin-cert.pem
-  cp $HOST_HOME/admin/msp/signcerts/cert.pem $HOST_HOME/$PEER2_HOST/msp/admincerts/admin-cert.pem
-  cp $HOST_HOME/admin/msp/signcerts/cert.pem $HOST_HOME/$ORDERER_HOST/msp/admincerts/admin-cert.pem
+  mkdir $HOST_HOME/$AGENT_HOST/msp/admincerts    
+  cp $HOST_HOME/$ADMIN_USER/msp/signcerts/cert.pem $HOST_HOME/$AGENT_HOST/msp/admincerts/admin-cert.pem  
 
 else
   echo "type not supplied!"
