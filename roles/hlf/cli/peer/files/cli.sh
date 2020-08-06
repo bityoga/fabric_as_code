07ce4f3927a923d7091b6e682e2e14e89ee12279
Antorweep Chakravorty <chakravrty.antorweep@gmail.com>
2020-08-06 19:38:39 +0200

    woring on peers

--- roles/hlf/cli/peer/files/cli.sh
+++ roles/hlf/cli/peer/files/cli.sh
@@ -4,14 +4,14 @@ CORE_PEER_MSPCONFIGPATH=/root/${ADMIN_USER}/msp
 if (($IDX == 0)); then    
     
     # Create the application channel    
-    CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH peer channel create -c appchannel -f /root/${AGENT_HOST}_cli/artifacts/appchannel.tx -o ${ORDERER_HOST}:7050 --outputBlock /root/${AGENT_HOST}_cli/artifacts/appchannel.block --tls --cafile ${CORE_PEER_TLS_ROOTCERT_FILE}
+    CORE_PEER_ADDRESS=$CORE_PEER_ADDRESS CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH peer channel create -c appchannel -f /root/${AGENT_HOST}_cli/artifacts/appchannel.tx -o ${ORDERER_HOST}:7050 --outputBlock /root/${AGENT_HOST}_cli/artifacts/appchannel.block --tls --cafile ${CORE_PEER_TLS_ROOTCERT_FILE}
 
     #Update the channel with anchor peers
-    CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH peer channel update -o ${ORDERER_HOST}:7050 -c appchannel -f /root/${AGENT_HOST}_cli/artifacts/appchannel_anchor.tx --tls --cafile ${CORE_PEER_TLS_ROOTCERT_FILE}
+    CORE_PEER_ADDRESS=$CORE_PEER_ADDRESS CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH peer channel update -o ${ORDERER_HOST}:7050 -c appchannel -f /root/${AGENT_HOST}_cli/artifacts/appchannel_anchor.tx --tls --cafile ${CORE_PEER_TLS_ROOTCERT_FILE}
 fi
 
 # Join the peers to the application channel
-CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH CORE_PEER_ADDRESS=${AGENT_HOST}:7051 CORE_PEER_TLS_ROOTCERT_FILE=$CORE_PEER_TLS_ROOTCERT_FILE peer channel join -b /root/${AGENT_HOST}_cli/artifacts/appchannel.block
+CORE_PEER_ADDRESS=$CORE_PEER_ADDRESS CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH CORE_PEER_ADDRESS=${AGENT_HOST}:7051 CORE_PEER_TLS_ROOTCERT_FILE=$CORE_PEER_TLS_ROOTCERT_FILE peer channel join -b /root/${AGENT_HOST}_cli/artifacts/appchannel.block
 
 
 while true; do
