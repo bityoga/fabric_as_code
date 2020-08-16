d55515278e8b94ea0b6011f841f3f2082cd521f6
achak1987 <chakravorty.antorweep@gmail.com>
2020-08-16 18:29:28 +0200

    upgrade to 2.2 complete. AUtomated ChainCode Depployment

--- start_hlf_services.sh
+++ start_hlf_services.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x #echo on
 
-ansible-playbook -v 100.deploy_ca.yml --flush-cache -u root 
-ansible-playbook -v 101.deploy_orderer.yml --flush-cache -u root 
-ansible-playbook -v 102.deploy_peers.yml --flush-cache -u root
-ansible-playbook -v 103.deploy_cli.yml --flush-cache -u root
-ansible-playbook -v 104.deploy_hlf_explorer.yml --flush-cache -u root
+ansible-playbook -v 100.deploy_ca.yml --flush-cache -u root;
+ansible-playbook -v 101.deploy_orderer.yml --flush-cache -u root;
+ansible-playbook -v 102.deploy_peers.yml --flush-cache -u root;
+ansible-playbook -v 103.deploy_cli.yml --flush-cache -u root;
+ansible-playbook -v 104.deploy_hlf_explorer.yml --flush-cache -u root;
