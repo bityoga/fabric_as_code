fce5500533b9ecc442eb14775081f18f4a3275ee
anandhakumarpalanisamy <anandhakumarpalanisamy91@gmail.com>
2020-10-19 13:49:55 +0200

    - ignore errors

--- roles/hlf/cli/cli/files/CLI.sh
+++ roles/hlf/cli/cli/files/CLI.sh
@@ -2,10 +2,10 @@
 #!/bin/bash
 set -x #echo on
 if [ $INSTALL_BANK_CHAINCODE == "y" ]; then
-    rm -rfv /root/CLI/chaincodes/articonf-bank-chaincode &&
-    git clone https://github.com/bityoga/articonf-bank-chaincode.git /root/CLI/chaincodes/articonf-bank-chaincode && 
-    bash /root/CLI/chaincodes/articonf-bank-chaincode/bank_chaincode/shell_scripts_v2/install.sh && 
-    bash /root/CLI/chaincodes/articonf-bank-chaincode/bank_chaincode/shell_scripts_v2/instantiate.sh && 
+    rm -rfv /root/CLI/chaincodes/articonf-bank-chaincode || true &&
+    git clone https://github.com/bityoga/articonf-bank-chaincode.git /root/CLI/chaincodes/articonf-bank-chaincode || true &&
+    bash /root/CLI/chaincodes/articonf-bank-chaincode/bank_chaincode/shell_scripts_v2/install.sh || true &&
+    bash /root/CLI/chaincodes/articonf-bank-chaincode/bank_chaincode/shell_scripts_v2/instantiate.sh || true &&
     while true; do sleep 2; done;
 else
     while true; do sleep 2; done;
