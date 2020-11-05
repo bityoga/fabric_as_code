8754e365024a1d40e0b38bed1f70ad55c648584c
anandhakumarpalanisamy <anandhakumarpalanisamy91@gmail.com>
2020-11-05 03:39:16 +0100

    - removed unwanted sleep from start_all_services.sh and start_hlf_services.sh

--- start_all_services.sh
+++ start_all_services.sh
@@ -7,8 +7,8 @@ ansible-playbook -v 013.mount_fs.yml -u root &&
 ansible-playbook -v 014.spawn_swarm.yml -u root &&
 ansible-playbook -v 015.deploy_swarm_visualizer.yml --flush-cache -u root &&
 ansible-playbook -v 016.deploy_portainer.yml --flush-cache -u root &&
-ansible-playbook -v 100.deploy_ca.yml --flush-cache -u root && sleep 15 &&
-ansible-playbook -v 101.deploy_orderer.yml --flush-cache -u root && sleep 10 &&
-ansible-playbook -v 102.deploy_peers.yml --flush-cache -u root && sleep 10 &&
-ansible-playbook -v 103.deploy_cli.yml --flush-cache -u root && sleep 10 &&
+ansible-playbook -v 100.deploy_ca.yml --flush-cache -u root &&
+ansible-playbook -v 101.deploy_orderer.yml --flush-cache -u root &&
+ansible-playbook -v 102.deploy_peers.yml --flush-cache -u root &&
+ansible-playbook -v 103.deploy_cli.yml --flush-cache -u root &&
 ansible-playbook -v 104.deploy_hlf_explorer.yml --flush-cache -u root
